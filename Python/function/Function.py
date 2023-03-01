b4194b15e1cdc06fdad8e241798e97332638da34
Rushikesh5686 <rushibundhe2020@gmail.com>
2023-03-01 16:41:56 +0530

    file

---
+++ Python/function/Function.py
@@ -0,0 +1,19 @@
+def add(x,y):
+    return x+y
+
+def mul(x,y):
+    return x*y
+
+def sub(x,y):
+    return x-y
+
+def div(x,y):
+    return x/y
+a=int(input("Enter your nuber"))
+b=int(input("Enter your nuber"))
+
+print('Addition:',add(a,b))
+print('Subtraction:',sub(a,b))
+print('Division:',div(a,b))
+print('Multiplication:',mul(a,b))
+
